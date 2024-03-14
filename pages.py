0d5a59ce6ae40dbf224003afa94c18fbd0581e5e
G0LDF0X <astnv31@gmail.com>
2024-03-14 15:33:29 +0900

    Ver 1.1.0 updated. Add period_all() function in pages.py

--- pages.py
+++ pages.py
@@ -22,7 +22,7 @@ line_list = data["호선"].unique()
 station_list = data["역명"].unique()
 time_list = data.columns[6:-1]
 color_data = {"1호선":"#0032A0", "2호선":"#00B140", "3호선":"#FC4C02", "4호선":"#00A9E0", "5호선":"#A05EB5",
-              "6호선":"#A9431E", "7호선":"#67823A", "8호선":"E31C79", "혼잡도 34%": "#FF0000"}
+              "6호선":"#A9431E", "7호선":"#67823A", "8호선":"#E31C79", "혼잡도 34%": "#FF0000"}
 
 def txt_gen(txt):
     for t in list(txt):
@@ -76,7 +76,7 @@ def period():
     matplot_data = graph_data.T
 
     fig = plt.figure()
-    plt.title("{} 시간에 따른 지하철 복잡도".format(search_date))
+    plt.title("{} 시간에 따른 {} 지하철 복잡도".format(search_date, station_select))
     plt.xlabel("시간")
     plt.ylabel("복잡도")
     for line in line_selects:
@@ -121,6 +121,41 @@ def period():
 # 시간별(전체)
 def period_all():
     st.title("전체 역의 시간별 분석")
+    st.info("해당 데이터는 각 호선의 평균을 사용했습니다.")
+    
+    # 날짜 선택
+    search_date = datetime.today()
+    search_date = st.date_input("검색하고자 하는 날짜를 입력해주세요.", search_date)
+    st.caption("날짜를 선택하지 않을 경우 기본적으로 오늘을 선택합니다.")
+
+    # 검색하는 날짜가 평일인지 토요일인지 일요일인지 체크
+    if search_date.weekday() == 5:
+        weekday = "토요일"
+    elif search_date.weekday() == 6:
+        weekday = "일요일"
+    else:
+        weekday = "평일"
+
+    data_today = data[data["요일구분"] == weekday]
+
+    # 호선 선택
+    line_selects = st.multiselect("호선을 선택해주세요.", data_today["호선"].unique(), placeholder="평균을 확인 할 지하철 호선")
+    fig = plt.figure()
+    plt.title("{} 시간에 따른 전체 역 평균 지하철 복잡도".format(search_date))
+    plt.xlabel("시간")
+    plt.ylabel("복잡도")
+
+    # 호선을 여러 개 선택했을 경우
+    for line in line_selects:
+        graph_data = data_today[data_today["호선"] == line].iloc[:, 6:-1]
+        matplot_data = graph_data.describe().T[["mean"]]
+        plt.plot(matplot_data.index, matplot_data["mean"], label = "{} 평균".format(line), color=color_data[line])
+
+    plt.xticks(fontsize=5, rotation=45)
+    plt.yticks(fontsize=7)
+    plt.plot(time_list, [34.0] * len(time_list), label="혼잡도 34%", linestyle=":", color="red")
+    plt.legend(loc="best")
+    st.pyplot(fig)
     pass
 
 # 호선별(특정 역)
