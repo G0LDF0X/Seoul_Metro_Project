84f75e84bce0153efa111a521783339a5e320e84
G0LDF0X <astnv31@gmail.com>
2024-03-15 09:07:40 +0900

    Update README.md

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if "page" not in st.session_state:
     st.session_state["page"] = "HOME"
 
 menus = {"HOME": home, "시간별(특정 역)": period, "시간별(전체)": period_all,"호선별(특정 역)": line,
-          "역별(특정 호선)": line_all, "역 별(전체)": station_all}
+          "역별(특정 호선)": line_all, "역별(전체)": station_all}
 with st.sidebar:
     for menu in menus:
         if st.button(menu, use_container_width=True, type = "primary" if st.session_state["page"] == menu else "secondary"):
@@ -16,6 +16,8 @@ with st.sidebar:
 st.sidebar.title("TECHIT 백엔드 스쿨 9기")
 st.sidebar.subheader("서울 지하철 혼잡도 데이터 대시보드")
 st.sidebar.text("1차 프로젝트")
+st.sidebar.link_button(":computer: GitHub", "https://github.com/G0LDF0X/Seoul_Metro_Project")
+st.sidebar.video("https://youtu.be/LseQZXXBXv8?si=61NpsjHyOP9qHY2d")
 
 for menu in menus.keys():
     if st.session_state["page"] == menu:
